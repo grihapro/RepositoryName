4852475686cdef33e47efd18aaa185f6d10c035d
grihapro <147150598+grihapro@users.noreply.github.com>
2023-10-23 20:08:50 +0300

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,13 @@
+def find_common_participants(string1, string2, space=','):
+    _list1 = string1.split(space)
+    _list2 = string2.split(space)
+    _set = set(_list1)
+    set_common = _set.intersection(_list2)
+    list_common = list(set_common)
+    return list_common
+
+
+participants_first_group = "Иванов|Петров|Сидоров"
+participants_second_group = "Петров|Сидоров|Смирнов"
+
+# TODO Провеьте работу функции с разделителем отличным от запятой
